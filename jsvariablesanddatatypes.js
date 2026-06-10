83c6c99616681ea8cd1762787450ec59c7c92f95
Shubham Dixit <shubhdixit19@gmail.com>
2026-06-11 01:39:59 +0530

    Initial JavaScript project

---
+++ jsvariablesanddatatypes.js
@@ -0,0 +1,61 @@
+//Multiple line
+//var x; //decleration
+//x=10;  //intialization
+
+//single statement decleration+initialization
+//var x=10;
+
+//single statement - multiple variable
+// var x = 100,y=200;
+
+// document.write(x)
+// console.log(x)
+
+//*****Let */
+
+//Multiple line decleration, initialization
+
+//let x; //decleration
+//x="welcome"; //initialization
+
+//single statement - decleration+initialization
+//let x="welcome"
+//console.log(x);
+//document.write(x);
+
+//Multiple variable in single line
+// var x="welcome", y=200;
+// console.log(x);
+// console.log(y);
+
+//Difference between Var and let
+
+// console.log(x);
+// //var x=200;
+// let x=100;
+// console.log(x);
+
+//*****const */
+
+// const x=100;
+// console.log(x);
+// const x=500; //incorrect because x is const
+// console.log(x);
+
+//*****Data types
+
+// let x=2;
+// console.log(typeof(x)); //number
+// let price=100.50;
+// console.log(typeof(price)); //number
+// let student = "Rajeev"
+// console.log(typeof(student)); //String
+
+// let flag=true;
+// console.log(typeof(flag)); //Boolean
+
+// let status = null;
+// console.log(typeof(status)) //Object
+
+// let value = undefined;
+// console.log(typeof(value)); //undefined
