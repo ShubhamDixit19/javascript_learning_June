78455df21631c9add19b9f1e7bdcf9096c0fe02b
Shubham Dixit <shubhdixit19@gmail.com>
2026-06-21 15:30:57 +0530

    changesloops

---
+++ forloopdemo.js
@@ -0,0 +1,20 @@
+//1,2,3.......10 numbers
+
+// for(let i=1; i<=10; i++)
+// {
+//     console.log(i);
+// }
+
+//10,9,8,7......0
+
+// for(let i=10; i>0; i--)
+// {
+//     console.log(i);
+// }
+
+//Even number b/w 1 to 10
+
+// for(let i = 2; i<=10; i+=2)
+// {
+//     console.log(i);
+// }
