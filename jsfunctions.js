fc4d87a43141d7fddaedfd49e98a945a307891bd
Shubham Dixit <shubhdixit19@gmail.com>
2026-06-22 16:02:32 +0530

    new changes cal and functions

---
+++ jsfunctions.js
@@ -0,0 +1,32 @@
+//example 1
+
+// function add(a,b)
+// {
+//     console.log(a+b);
+// }
+// add(100,200);
+
+//example 2
+
+// function add(a,b)
+// {
+//     return(a+b);
+// }
+
+// let res = add(10,30)
+// console.log(res);
+
+//example 3 
+
+function greeting()
+{
+    console.log("Hey Good Morning")
+}
+greeting();
+
+
+function greetings()
+{
+    return("Hey Good Morning")
+}
+console.log(greetings());
